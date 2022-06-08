db52202fe16424acf8d2e4859a818cc86873837c
arthurvmbl <arthurvmbl@gmail.com>
2022-06-08 00:10:07 -0300

    Create passwordgeneratorbyinputbeta.py

---
+++ passwordgeneratorbyinputbeta.py
@@ -0,0 +1,63 @@
+from asyncore import write
+from fileinput import close
+import random
+import itertools
+from itertools import product
+import ssl
+import sys
+
+
+
+print("""\
+    
+    
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@                                                                                                                                             @@@@@
+@@@@  @@@@@@@@@@@@@        @@@@@@@@@@@      @@@@       @@@@    @@@@@@@@@@@@               @@@@@@@@@@@      @@@@@@@@@@@@@@@    @@@@       @@@@    @@@@@
+@@@@  @@@@       @@@@    @@@@               @@@@       @@@@    @@@@       @@@           @@@@               @@@@               @@@@@@@@@  @@@@    @@@@@
+@@@@  @@@@       @@@@      @@@@@@@@@@@      @@@@   @@  @@@@    @@@@       @@@@  @@@@@@  @@@@     @@@@@@    @@@@@@@@@@@@@      @@@@   @@@@@@@@    @@@@@
+@@@@  @@@@@@@@@@@@@                 @@@@    @@@@@@@@@@@@@@@    @@@@       @@@@  @@@@@@  @@@@       @@@@    @@@@               @@@@     @@@@@@    @@@@@
+@@@@  @@@@               @@@@       @@@@    @@@@@@   @@@@@@    @@@@     @@@@            @@@@       @@@@    @@@@               @@@@       @@@@    @@@@@
+@@@@  @@@@                 @@@@@@@@@@@      @@@@       @@@@    @@@@@@@@@@@                @@@@@@@@@@  @    @@@@@@@@@@@@@@@    @@@@       @@@@    @@@@@
+@@@@                                                                                                                                             @@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+by arthurvmbl   
+
+    """)
+
+while True:
+    namelist = input("Give your list a name: ")
+    f = open(namelist + ".txt","w+")
+    name = input("Name; ")
+    nick = input("Nickname; ")
+    yearbirth = input("Year of Birth; ")
+    relative1 = input("Relative; ")
+    relative2 = input("Relative; ")
+    relative3 = input("Relative; ")
+    relative4 = input("Relative; ")
+    favoriteword = input("Favorite Word: ")
+    id = input("Id: ")
+    phone = input("Phone; ")
+    oldphone = input("Old Phone; ")
+    ss = input("Personal Doc: ")
+    dobw = input("Year of Birth of Spouse; ")
+    pet = input("Pet; ")
+    job = input("Job; ")
+    team = input("Team/Sport; ")
+
+    lista = [name, nick, yearbirth, relative1, relative2, relative3, relative4, favoriteword, phone,
+             oldphone, id, ss, dobw, pet, job, team]
+
+    lista = list(filter(None, lista))
+    
+    print(len(lista))
+
+    tamain = int(input("How many words do you want to combine? "))
+
+    s = lista
+    t = list(itertools.permutations(s, tamain))
+    for i in range(0, len(t)):
+            print(''.join(t[i]), file=f)
