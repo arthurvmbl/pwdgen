c0d0ad56ce85386ae2fc3b84128e9c52f0b3c5c8
victormedeiros96 <victormedeiros96@gmail.com>
2022-06-30 23:35:32 -0300

    Add encode text feature.

--- passwordgeneratorbyinputbeta.py
+++ passwordgeneratorbyinputbeta.py
@@ -19,7 +19,8 @@ by arthurvmbl & HenriqueMenezesN
     """)
 from itertools import permutations
 from math import floor
-def list_generator(list_of_possible_words,namelist):
+def list_generator(list_of_possible_words,namelist,encode):
+    if not encode:encode='utf-8'
     try:
         with open(namelist + ".txt",mode="wb") as f:
             list_of_possible_words = list(filter(None,list_of_possible_words))
@@ -39,9 +40,9 @@ def list_generator(list_of_possible_words,namelist):
             print(f'Batch size: '+str(batch_size)+' and '+str(len(permutation_examples))+' rows'  if batch_size<len(permutation_examples) else 'Batch size: '+str(len(permutation_examples)))
             last = floor(len(permutation_examples)/batch_size)
             for i in range(last):
-                output = [(''.join(_)+'\n').encode() for _ in permutation_examples[batch_size*i:batch_size*(i+1)]]
+                output = [(''.join(_)+'\n').encode(encode) for _ in permutation_examples[batch_size*i:batch_size*(i+1)]]
                 f.writelines(output)
-            output = [(''.join(_)+'\n').encode() for _ in permutation_examples[batch_size*last:]]
+            output = [(''.join(_)+'\n').encode(encode) for _ in permutation_examples[batch_size*last:]]
             f.writelines(output)
         return True
     except:return False
@@ -49,7 +50,13 @@ if __name__=='__main__':
     namelist = input("Give your list a name: ")
     lista = 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'.split(' ')
     
-    if list_generator(lista,namelist):
+    encodings = ['unicode_escape','raw_unicode_escape','utf-8','ascii']
+    for a,b in enumerate(encodings):print("",a,b,sep='\t')
+    encode = input("\t>> ")
+    try:encodings = encodings[int(encode)]
+    except:encodings = None
+    
+    if list_generator(lista,namelist,encodings):
         print("Your list is ready!")
     else:
         print('Error on list creation.')
